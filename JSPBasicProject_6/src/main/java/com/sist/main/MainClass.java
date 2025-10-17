1c2a5b5556fe050b11bcf421f8bbfa759493e8b4
dongdongsw <fishman4535@gmail.com>
2025-10-17 17:00:19 +0900

    16일차

---
+++ JSPBasicProject_6/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,17 @@
+package com.sist.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Map map = new HashMap();
+		map.put("name", "홍길동");
+		System.out.println(map.get("name"));
+		map.put("name", "심청이");
+		System.out.println(map.get("name"));
+	}
+
+}
